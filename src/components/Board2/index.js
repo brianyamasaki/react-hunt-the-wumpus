0a4a1bee05d9e8da5bb1e7f15578f1b23be45cbc
Brian Yamasaki <brian.yamasaki@gmail.com>
2018-03-29 20:07:19 -0700

    Board2 draws correctly

--- src/components/Board2/index.js
+++ src/components/Board2/index.js
@@ -59,7 +59,7 @@ class Board extends Component {
         key={i}
         style={style}
       >
-        <p style={{ marginTop: size / 4}}>{room.iCave}</p>
+        <p style={{ marginTop: size / 4}}>{room.iRoom}</p>
       </div>
     );
   }
@@ -77,6 +77,55 @@ class Board extends Component {
     }
   }
 
+  renderPath(origin, connection, i, stuff) {
+    return (
+      <line 
+        key={i}
+        x1={ origin.x * stuff.widthFactor} 
+        y1={ origin.y * stuff.heightFactor} 
+        x2={ connection.x * stuff.widthFactor}
+        y2={ connection.y * stuff.heightFactor} 
+        strokeWidth={stuff.strokeWidth} 
+        stroke="white" 
+      />
+    );
+  }
+
+  renderRoomPaths(mazeRoom, i, stuff) {
+    // find the room in the board data
+    const originBoardRoom = stuff.boardRooms.find(room => room.real && room.iRoom === mazeRoom.room + 1);
+    const neighbors = originBoardRoom.neighbors;
+    // collect all board neighbors in array
+    const boardNeighbors = stuff.boardRooms.filter(room => neighbors.indexOf(room.index) !== -1 );
+    // find board rooms connected by this mazeRoom in an array
+    const connectedNeighbors = boardNeighbors.filter(room => mazeRoom.connections.indexOf(room.iRoom - 1) !== -1 );
+    console.log(mazeRoom.room + 1, originBoardRoom.iRoom, boardNeighbors, connectedNeighbors.map(room => room.iRoom));
+    return connectedNeighbors.map((connection, index) => this.renderPath(originBoardRoom, connection, i*100+index, stuff));
+  }
+
+  renderPaths() {
+    const { imgWidth, imgHeight } = this.state;
+    const { maze } = this.props;
+    const viewBox = `0 0 ${imgWidth} ${imgHeight}`;
+    if (imgWidth) {
+      const stuff = {
+        widthFactor: imgWidth / boardData.widthDenominator,
+        heightFactor: imgHeight / boardData.heightDenominator,
+        strokeWidth: imgWidth / 30,
+        boardRooms: boardData.rooms,
+        mazeRooms: maze
+      };
+      return (
+        <div className="paths">
+          <svg width={imgWidth} height={imgHeight} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
+            {maze.map((mazeRoom, i) => this.renderRoomPaths(mazeRoom, i, stuff))}
+          </svg>
+        </div>
+      );
+  
+    }
+  }
+
   render() {
     return (
       <div 
@@ -87,6 +136,7 @@ class Board extends Component {
           alt="Board Background" 
           onLoad={this.onImgLoad}
         />
+        {this.renderPaths()}
         {this.renderRooms()}
       </div>
     );
@@ -94,7 +144,10 @@ class Board extends Component {
 }
 
 const mapStateToProps = state => {
-  return {};
+  const { mazeData } = state;
+  return {
+    maze: mazeData.maze
+  };
 }
 
 export default connect(mapStateToProps)(Board);
